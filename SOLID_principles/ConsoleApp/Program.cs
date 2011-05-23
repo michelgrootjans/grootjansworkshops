440f25182099a31fb33c9d4a0d754f9da5b90e49
michel.grootjans <michel.grootjans@df3d06e4-e8d5-11dd-9059-71fac19e68c6>
2011-05-23 07:19:44 +0000

    Prepare SOLID_principles

---
+++ SOLID_principles/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DependencyInversion;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please start typing...");
+            new Copy().Run();
+        }
+    }
+}
