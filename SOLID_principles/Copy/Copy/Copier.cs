3ba4585fb06daea6fa365f5fbc452a56857ae4d0
michel.grootjans <michel.grootjans@df3d06e4-e8d5-11dd-9059-71fac19e68c6>
2011-06-07 21:13:00 +0000

    Added copy solution

---
+++ SOLID_principles/Copy/Copy/Copier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Copy
+{
+    public class Copier
+    {
+        public void Copy()
+        {
+            string c;
+            while ((c = Keyboard.Read()) != Environment.NewLine)
+            {
+                Printer.Write(c);
+            }
+        }
+    }
+}
---
+++ SOLID_principles/Copy/Copy/Keyboard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Copy
+{
+    public static class Keyboard
+    {
+        public static string Read()
+        {
+            Console.Write("Keyboard: ");
+            return Console.ReadLine();
+        }
+    }
+}
