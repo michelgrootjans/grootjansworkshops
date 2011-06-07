fcea45607d3652a1a7db5ff5772ad668d4d56659
michel.grootjans <michel.grootjans@df3d06e4-e8d5-11dd-9059-71fac19e68c6>
2011-06-07 21:13:00 +0000

    Added copy solution

---
+++ trunk/SOLID_principles/Copy/Copy/Printer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Copy
+{
+    public static class Printer
+    {
+        public static void Write(string text)
+        {
+            Console.WriteLine("Printer Prints: " + text);
+        }
+    }
+}
---
+++ trunk/SOLID_principles/Copy/Copy/Program.cs
@@ -0,0 +1,11 @@
+namespace Copy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var copier = new Copier();
+            copier.Copy();
+        }        
+    }
+}
